8382437d4ab5b6bbcc77014066981f3d90e34578
Vincent Barilla <vincentbarilla@Vincents-MacBook-Pro.local>
2016-07-11 17:34:29 -0700

    Test passed.
    Sublime 2 is confirmed as default text editor for git commits from new
    laptop's shell.

--- public/test.js
+++ public/test.js
@@ -1,1 +1,4 @@
 //test commit from new laptop
+//test successfully passed. Sublime 2 is confirmed as default text editor for (var i = // git commits..length - 1; i >= 0; i--) {
+	// git commits.[i]
+};
